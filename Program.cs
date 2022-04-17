8c55289df4ebf9887d165ed284f50f7c7c38ef7e
suraj807 <surajdev805@gmail.com>
2022-04-17 15:37:11 +0530

    createlinklist

--- Program.cs
+++ Program.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpLinkedListAssignment
+namespace LinkedListDataStructureAssignments
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" welcome to link list");
-           
+            LinkedList<int> list = new LinkedList<int>(); // creating a object of in built LinkedList generic class 
+            list.AddFirst(56); // Calling method of Linked List generic class to add 56 at first
+            list.AddLast(30); // Calling method of Linked List generic class to add 30 at last
+            list.AddLast(70); // Calling method of Linked List generic class to add 70 at last
+            foreach (int element in list)
+            {
+                Console.WriteLine(element); // Printing all the element of linked list
+            }
+            Console.ReadLine();
         }
     }
 }
